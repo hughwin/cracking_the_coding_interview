639820f94120f009b5f93fae220758f797ddb4bf
hughwin <hugh.winchester@gmail.com>
2021-04-07 20:59:05 +0100

    Added solution to isUnique

--- src/com/hughwin/Main.java
+++ src/com/hughwin/Main.java
@@ -1,7 +1,22 @@
 package com.hughwin;
 
+import java.util.HashSet;
+
 public class Main {
 
     public static void main(String[] args) {
+
     }
+
+    //1.1 Is Unique
+
+    public boolean isUnique(String str){
+        HashSet<Character> uniques = new HashSet<>();
+        char[] charArray = str.toCharArray();
+        for(char c : charArray){
+            if(!uniques.add(c)) return false;
+        }
+        return true;
+    }
+
 }
